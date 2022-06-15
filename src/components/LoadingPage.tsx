bcfb5413191bdb1ac56fbde5780340879a05835c
Nicolas <pnwa@icloud.com>
2022-06-15 17:31:09 +0200

    add loaders

---
+++ src/components/LoadingPage.tsx
@@ -0,0 +1,11 @@
+import Loader from "./Loader"
+
+const LoadingPage = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Loader />
+    </div>
+  )
+}
+
+export default LoadingPage
