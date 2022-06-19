e44b535f26692ad73cd8d7da73367ef3690ee6a1
Nicolas <pnwa@icloud.com>
2022-06-19 20:55:39 +0200

    tent card and tent informations lines

---
+++ src/components/TentCard.tsx
@@ -0,0 +1,36 @@
+import { Tent } from "@/types/graphql"
+import Link from "next/link"
+import TentInformation, { TentComplete, TentState } from "./TentInformation"
+
+const TentCard = ({ tent }: { tent: Tent }) => {
+  const { id, identifyingNum, unit, location, complete, state, size } = tent
+
+  return (
+    <article className="cursor-pointer rounded-lg border border-slate-900/10 shadow-lg transition-all hover:scale-[1.02]">
+      <Link href={`/tents/${id}`}>
+        <a>
+          <div className="space-y-6 p-4 pb-5">
+            <div className="flex items-center space-x-3">
+              <div className="flex h-20 w-20 items-center justify-center rounded-full border-4 border-slate-800">
+                <h2 className="text-2xl font-bold">{identifyingNum}</h2>
+              </div>
+              <div>
+                <h3 className="text-lg font-bold leading-tight">
+                  {unit || "GROUPE"}
+                </h3>
+                <p className="text-sm font-semibold ">{size} places</p>
+              </div>
+            </div>
+            <div className="space-y-2">
+              <TentState value={state} />
+              <TentComplete value={complete} />
+              <TentInformation label="Où ?" value={location} />
+            </div>
+          </div>
+        </a>
+      </Link>
+    </article>
+  )
+}
+
+export default TentCard
---
+++ src/components/TentInformation.tsx
@@ -0,0 +1,64 @@
+import { State } from "@/types/graphql"
+import classNames from "classnames"
+import { ReactNode } from "react"
+
+const LabelWrapper = ({ children }: { children: ReactNode }) => {
+  return (
+    <span className="w-[90%] rounded-md rounded-r-none bg-slate-600 px-1 py-2 text-white">
+      {children}
+    </span>
+  )
+}
+const TentInformation = ({ label, value }: { label: string; value: any }) => {
+  return (
+    <div className="flex items-center justify-between rounded-md bg-gray-200 text-center font-semibold text-black">
+      <LabelWrapper>{label}</LabelWrapper>
+      <span className="w-full rounded-md rounded-l-none px-1 py-1">
+        {value}
+      </span>
+    </div>
+  )
+}
+
+export const TentComplete = ({ value }: { value: boolean }) => {
+  return (
+    <div
+      className={classNames(
+        "flex items-center justify-between rounded-md text-center font-semibold text-white",
+        {
+          "bg-green-400": value,
+          "bg-red-400": !value,
+        }
+      )}
+    >
+      <LabelWrapper>Complète ?</LabelWrapper>
+      <span className="w-full rounded-md rounded-l-none px-1 py-1">
+        {value ? "OUI" : "NON"}
+      </span>
+    </div>
+  )
+}
+
+export const TentState = ({ value }: { value: State }) => {
+  return (
+    <div
+      className={classNames(
+        "flex items-center justify-between rounded-md text-center font-semibold text-white",
+        {
+          "bg-green-500": value === "NEUF",
+          "bg-lime-500": value === "BON",
+          "bg-yellow-300": value === "OK",
+          "bg-orange-500": value === "MAUVAIS",
+          "bg-red-500": value === "INUTILISABLE",
+        }
+      )}
+    >
+      <LabelWrapper>État</LabelWrapper>
+      <span className="w-full rounded-md rounded-l-none px-1 py-1">
+        {value}
+      </span>
+    </div>
+  )
+}
+
+export default TentInformation
