2616baf8f9bf116f352df269a5394758128d9491
Nicolas <pnwa@icloud.com>
2022-06-20 21:28:01 +0200

    set default cache policy to no cache

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,16 +1,28 @@
 import {
   ApolloClient,
+  DefaultOptions,
   InMemoryCache,
   NormalizedCacheObject,
 } from "@apollo/client"
 
 let apolloClient: ApolloClient<NormalizedCacheObject>
+const defaultOptions: DefaultOptions = {
+  watchQuery: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "ignore",
+  },
+  query: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "all",
+  },
+}
 
 if (process.env.NODE_ENV === "production") {
   apolloClient = new ApolloClient({
     uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
     cache: new InMemoryCache(),
     ssrMode: typeof window === "undefined",
+    defaultOptions,
   })
 } else {
   if (!global.apolloClient) {
@@ -18,6 +30,7 @@ if (process.env.NODE_ENV === "production") {
       uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
       cache: new InMemoryCache(),
       ssrMode: typeof window === "undefined",
+      defaultOptions,
     })
   }
 
@@ -30,4 +43,5 @@ export const client = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
   cache: new InMemoryCache(),
   ssrMode: typeof window === "undefined",
+  defaultOptions,
 })
