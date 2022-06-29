5defa82ba63dbbee3e7cd2082f3a761be3c6ec0e
Nicolas <pnwa@icloud.com>
2022-06-29 17:06:45 +0200

    up styling

--- src/components/TentDeleteForm.tsx
+++ src/components/TentDeleteForm.tsx
@@ -46,7 +46,7 @@ const TentDeleteForm = ({ tent }: { tent: Tent }) => {
   const handleDelete = () => deleteTent()
 
   return (
-    <div className="flex flex-col items-center space-y-8">
+    <div className="flex flex-col items-center space-y-8 py-5">
       <h2 className="flex items-center space-x-3">
         <div className="text-3xl font-bold sm:text-5xl">Tente n°</div>
         <div className="flex h-20 w-20 items-center justify-center rounded-full border-4 border-slate-800 sm:h-24 sm:w-24">
@@ -64,23 +64,25 @@ const TentDeleteForm = ({ tent }: { tent: Tent }) => {
         <p className="text-sm italic">Cette action est irreversible</p>
       </div>
 
-      <button
-        type="button"
-        onClick={handleDelete}
-        disabled={submitting}
-        className="flex w-fit items-center space-x-2 rounded-3xl bg-red-500 px-5 py-2 text-lg text-white shadow-lg transition hover:scale-[0.98] hover:shadow-sm disabled:bg-red-400"
-      >
-        <FaTrashAlt />
-        <span>Supprimer</span>
-      </button>
-      <button
-        type="button"
-        onClick={handleCancel}
-        className="flex w-fit items-center space-x-2 rounded-3xl bg-gray-400 px-5 py-2 text-lg text-white shadow-lg transition hover:scale-[0.98] hover:shadow-sm"
-      >
-        <FaArrowLeft />
-        <span>Annuler</span>
-      </button>
+      <div className="flex items-center justify-between space-x-5">
+        <button
+          type="button"
+          onClick={handleCancel}
+          className="flex w-fit items-center space-x-4 rounded-3xl bg-black px-5 py-2 text-lg text-white shadow-lg transition hover:scale-[0.98] hover:shadow-sm"
+        >
+          <FaArrowLeft />
+          <span>Annuler</span>
+        </button>
+        <button
+          type="button"
+          onClick={handleDelete}
+          disabled={submitting}
+          className="flex w-fit items-center space-x-3 rounded-3xl bg-red-500 px-5 py-2 text-lg text-white shadow-lg transition hover:scale-[0.98] hover:shadow-sm disabled:bg-red-400"
+        >
+          <FaTrashAlt />
+          <span>Supprimer</span>
+        </button>
+      </div>
     </div>
   )
 }
