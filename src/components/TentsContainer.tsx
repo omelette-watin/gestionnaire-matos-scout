16bd146c5d2479a10d5b996a16f25e24079cf0c9
Nicolas <pnwa@icloud.com>
2022-07-02 02:23:05 +0200

    up style

--- src/components/TentsContainer.tsx
+++ src/components/TentsContainer.tsx
@@ -6,7 +6,7 @@ import { IoMdClose } from "react-icons/io"
 import ExcelExport from "./ExcelExport"
 import { FaPlus } from "react-icons/fa"
 import Link from "next/link"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { filterArr } from "@/lib/utilsFns"
 import classNames from "classnames"
 import { TentSizeInput, TentStateInput, TentUnitInput } from "./TentInputs"
@@ -41,8 +41,18 @@ const TentsContainer = ({ tents }: { tents: Tent[] }) => {
   }
   const toggleFilterVisibility = () => setShowFilters((prev) => !prev)
 
+  useEffect(() => {
+    if (showFilters) {
+      document.body.style.overflow = "hidden"
+    }
+
+    return () => {
+      document.body.style.overflow = "unset"
+    }
+  }, [showFilters])
+
   return (
-    <div>
+    <div className="pb-10">
       <div className="mt-10 flex flex-col font-medium lg:mt-5 lg:flex-row lg:justify-between">
         <div className="order-2 flex items-center space-x-4 lg:order-1">
           <button
@@ -53,13 +63,6 @@ const TentsContainer = ({ tents }: { tents: Tent[] }) => {
             <MdFilterListAlt />
             <span>Filtrer</span>
           </button>
-          {/* <button
-            type="button"
-            className="flex w-fit items-center space-x-2 rounded-3xl bg-blue-500 px-5 py-2 text-lg text-white shadow-lg transition hover:scale-[0.98] hover:shadow-sm"
-          >
-            <TbArrowsSort />
-            <span>Trier par</span>
-          </button> */}
         </div>
         <div className="order-1 mb-5 flex flex-col space-y-5 sm:flex-row sm:items-center sm:space-y-0 sm:space-x-4 lg:order-2 lg:mb-0">
           <ExcelExport tents={tents} />
@@ -83,7 +86,7 @@ const TentsContainer = ({ tents }: { tents: Tent[] }) => {
       )}
       <div
         className={classNames(
-          "fixed inset-0 z-50 items-center justify-center bg-black/30 text-white",
+          "fixed inset-0 z-50 items-start justify-center overflow-y-scroll overscroll-contain bg-black/30 text-white",
           {
             hidden: !showFilters,
             flex: showFilters,
@@ -92,7 +95,7 @@ const TentsContainer = ({ tents }: { tents: Tent[] }) => {
         onClick={toggleFilterVisibility}
       >
         <div
-          className="h-full w-full bg-white p-4 text-black sm:h-fit sm:w-[400px] sm:rounded-lg sm:shadow-lg"
+          className="h-fit min-h-screen w-full bg-white p-4 pb-6 text-black sm:mt-24 sm:h-fit sm:min-h-0 sm:w-[400px] sm:rounded-lg sm:shadow-lg"
           onClick={(e) => e.stopPropagation()}
         >
           <div className="flex justify-end">
@@ -132,10 +135,10 @@ const TentsContainer = ({ tents }: { tents: Tent[] }) => {
                 <span>Appliquer les filtres</span>
               </button>
               <div
-                className="mt-5 flex w-fit cursor-pointer items-center space-x-2 rounded-full bg-black p-2 text-lg text-white shadow-lg transition hover:scale-[0.98] hover:shadow-sm"
+                className="mt-5 flex w-fit cursor-pointer items-center space-x-2 rounded-full bg-black p-3 text-lg text-white shadow-lg transition hover:scale-[0.98] hover:shadow-sm"
                 onClick={cleanFilters}
               >
-                <MdRefresh />
+                <MdRefresh size={20} />
               </div>
             </div>
           </div>
